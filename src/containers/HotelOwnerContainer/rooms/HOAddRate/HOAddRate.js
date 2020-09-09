53710de543156db4d423349def0fa6b0b8e5101f
amir-schnabel <amir.schnabel.work@outlook.com>
2020-09-09 11:20:10 +0300

    Add rates screens

--- src/containers/HotelOwnerContainer/rooms/HOAddRate/HOAddRate.js
+++ src/containers/HotelOwnerContainer/rooms/HOAddRate/HOAddRate.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RMTable from '../../../../components/RMTable';
+import { getClasses } from '../../../App/RoommountUtil.js';
+
+const content = {
+    data: [{
+        key: "rates_types", topic: "Room Rates", headers: [
+            { name: "Plan" }, { name: "Start Date" }, { name: "End Date" }, { name: "Occupancy" },
+            { name: "Breakfast included" }, { name: "Free Of Charge" }, { name: "Price" }, { name: "Weekly Price" }],
+        items:
+            [{
+                key: "Plan259", Plan: "259", start_date: "1/1/2020", end_date: "31/12/2020", occupancy: "100", breakfest_included: "true", free_of_charge: "false",
+                price: ['{"type": "ref",         "href": "HOAddRoom"         }'],
+                weekly_price: ['{"type":"ref",        "href": "HODeleteRoomType"  }']
+            },
+            ]
+    }]
+}
 
 class HOAddRate extends React.Component {
+    getTables = () => {
+        let map = [];
+        let fields = ["Plan", "start_date", "end_date", "occupancy", "breakfest_included", "free_of_charge", "price", "weekly_price"];
+        content.data.forEach((item) => {
+            if (item.topic !== "") {
+                map.push(
+                    <div key={item.key} className="RMHOTableTopic">{item.topic}</div>)
+                map.push(<RMTable tabledata={item} fields={fields} />)
+            }
+        });
+        return map;
+    }
 
     render() {
-        return <span>HOAddRate</span>
+        let displayClass = "RMHOContentTable";
+        return (
+            <div className={displayClass}>{this.getTables()}</div>
+        )
     }
 }
 
--- src/containers/HotelOwnerContainer/rooms/HOAddRoom/HOAddRoom.js
+++ src/containers/HotelOwnerContainer/rooms/HOAddRoom/HOAddRoom.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createForm} from '../../../App/RoommountUtil.js';
+
+
+const selectionList = [{ name: "air_condition", display_name: "Air Condition" },
+    { name: "bath_or_shower", display_name: "Bath or Shower" },
+    { name: "bathrobe", display_name: "Bathrobe" },
+    { name: "bathtub or Shower", display_name: "bathtub or Shower" },
+    { name: "Cable Channels", display_name: "Cable Channels" },
+]
+
+const content = [
+    { id: "art11", displayName: 'Room Type' },
+    { id: "art12", displayName: 'Remarks', },
+    { id: "art13", displayName: 'Max occupancy', },
+    { id: "art14", type: "MultiplSelectionList", choices:{selectionList}, displayName: 'Features & Amenities', },
+];
+
+
 
 class HOAddRoom extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...props,
+        }
+    }
+
     render() {
-        return <span>HOAddRoom</span>
+        let displayClass = "RMHOContentTable";
+        return (
+            <div className={displayClass}>
+                {createForm(content)}
+                <button className="RMHOActionButton">Save</button>
+            </div>
+        );
     }
 }
+
 
 ReactDOM.render(<HOAddRoom />, document.getElementById('root'));
 
